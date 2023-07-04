84963e99f663ecf269ceac171df727513fc955bb
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-04 16:52:39 +0200

    Adding paragraph before Menu list with React Fragments, so it will have more than one element in Menu's JSX.

--- src/index.js
+++ src/index.js
@@ -77,17 +77,21 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to chose from. All from
+            our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
-
       {/* <Pizza
         name="Pizza Spinachi"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
